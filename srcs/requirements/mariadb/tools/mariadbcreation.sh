49d133ce8e09c3a6e697eb31f88d8bcbc05c7c3c
Phap <nguyen_vietphap@hotmail.fr>
2023-05-19 17:36:46 +0200

    r

--- srcs/requirements/mariadb/tools/mariadbcreation.sh
+++ srcs/requirements/mariadb/tools/mariadbcreation.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 service mysql start
 echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root
-echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'$IP_WORDPRESS' IDENTIFIED BY '$SQL_PASSWORD';" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO '${SQL_USER}'@'$IP_WORDPRESS' IDENTIFIED BY '${SQL_PASSWORD}';" | mysql -u root
+echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
-
+if [ -f "/var/local/bin/filename.sql" ]; then
+    mysql -u $SQL_USER --password=$SQL_PASSWORD  wordpress < /var/local/bin/filename.sql
+    mv /var/local/bin/filename.sql /var/local/
+fi
 service mysql stop
-
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-#mysql -e "GRANT ALL PRIVILEGES ON . TO 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-#mysql -e "FLUSH PRIVILEGES;"
-#mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-
-#mysqladmin -u root shutdown
-
 exec mysqld
